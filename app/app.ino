d44652b5f14ede2d5e5a5c17e9a3ded19d1ddc1a
MartinRFZ <mflores50@ucol.mx>
2024-04-18 12:23:23 -0600

    feat: declaración de función WiFi

--- app/app.ino
+++ app/app.ino
@@ -2,6 +2,9 @@
 
 void setup() {
   Serial.begin(115200);
+  wifiConnection.init();
+
+  delay(2000);
 }
 
 void loop() {
---
+++ app/wifi.h
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+class WifiConnection {
+public:
+  const char *SSID = "";           // Nombre de la red
+  const char *PASSWORD = ""; // Contraseña de la red
+
+public:
+  void init(void);                  // Funcion para inicializar la conexion WiFi
+  String getConnectionStatus(void); // Funcion para obtener el estado de la conexión WiFi
+};
+
+void WifiConnection::init(void) {
+  WiFi.begin(SSID, PASSWORD);
+  delay(1000);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nSuccess WiFi connection");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+String WifiConnection::getConnectionStatus(void) {
+  if (WiFi.status() == WL_CONNECTED) {
+    return "Connected to WiFi";
+  } else {
+    return "WiFi connection failed!";
+  }
+}
